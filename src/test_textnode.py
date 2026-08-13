eb41d37cb265ab0210fdc5612c272bf66b244978
larrymacrich <105505178+larrymacrich@users.noreply.github.com>
2026-08-13 13:32:51 +0200

    added translation for text node to leaf nodes

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,6 +1,5 @@
 import unittest
-from textnode import TextNode, TextType
-
+from textnode import TextNode, TextType, text_node_to_html_node
 
 class TestTextNodeEquality(unittest.TestCase):
     """Tests covering equality (__eq__) logic across properties."""
@@ -67,6 +66,51 @@ class TestTextNodeRepresentation(unittest.TestCase):
             "TextNode(text='Header text', text_type='text', url=None)",
         )
 
+class TestTextNodeToHTML(unittest.TestCase):
+    """Tests for TextNode.text_node_to_html_node()"""
+
+    def test_text(self):
+        node = TextNode("This is a text node", TextType.TEXT)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, None)
+        self.assertEqual(html_node.value, "This is a text node")
+
+    def test_bold(self):
+        node = TextNode("This is bold text", TextType.BOLD)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "b")
+        self.assertEqual(html_node.value, "This is bold text")
+
+    def test_italic(self):
+        node = TextNode("This is italic text", TextType.ITALIC)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "i")
+        self.assertEqual(html_node.value, "This is italic text")
+
+    def test_code(self):
+        node = TextNode("print('hello')", TextType.CODE)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "code")
+        self.assertEqual(html_node.value, "print('hello')")
+
+    def test_link(self):
+        node = TextNode("Click here", TextType.LINK, "https://boot.dev")
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "a")
+        self.assertEqual(html_node.value, "Click here")
+        self.assertEqual(html_node.props, {"href": "https://boot.dev"})
+
+    def test_image(self):
+        node = TextNode("An alt description", TextType.IMAGE, "https://boot.dev/image.png")
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "img")
+        self.assertEqual(html_node.value, "")
+        self.assertEqual(html_node.props, {"src": "https://boot.dev/image.png", "alt": "An alt description"})
+
+    def test_invalid_type_raises(self):
+        node = TextNode("Bad type", "not_a_real_type")
+        with self.assertRaises(TypeError):
+            text_node_to_html_node(node)
 
 if __name__ == "__main__":
     unittest.main()
--- src/textnode.py
+++ src/textnode.py
@@ -1,5 +1,5 @@
 from enum import Enum
-
+from leafnode import LeafNode
 
 class TextType(Enum):
     TEXT = "text"
@@ -30,3 +30,23 @@ class TextNode():
              f"{type(self).__name__}(text={self.text!r}, text_type={self.text_type.value!r}, "
              f"url={self.url!r})"
         )
+
+def text_node_to_html_node(text_node: TextNode) -> LeafNode:
+    match text_node.text_type:
+        case TextType.TEXT:
+            return LeafNode(tag=None,value=text_node.text)
+        case TextType.BOLD:
+            return LeafNode(tag="b",value=text_node.text)
+        case TextType.ITALIC:
+            return LeafNode(tag="i",value=text_node.text)
+        case TextType.CODE:
+            return LeafNode(tag="code",value=text_node.text)
+        case TextType.LINK:
+            return LeafNode(tag="a",value=text_node.text, props={"href" : text_node.url})
+        case TextType.IMAGE:
+            return LeafNode(tag="img",value="", props={"src" : text_node.url, "alt": text_node.text})
+        case _:
+            raise TypeError(
+                f"{text_node.text_type} type is not supported"
+                f"for {type(text_node).__name__}"
+            )    
