951cb14fd439968a9276a8b87d9e2abd1effd84e
larrymacrich <105505178+larrymacrich@users.noreply.github.com>
2026-09-02 18:36:53 +0200

    added more pages and img resources

--- src/main.py
+++ src/main.py
@@ -4,10 +4,31 @@ from gencontent import generate_page
 
 def main():
     copystatic()
+
     generate_page(
         'content/index.md',
         'template.html',
         'public/index.html'
+    )
+    generate_page(
+        'content/blog/glorfindel/index.md',
+        'template.html',
+        'public/blog/glorfindel/index.html'
+    )
+    generate_page(
+        'content/blog/tom/index.md',
+        'template.html',
+        'public/blog/tom/index.html'
+    )
+    generate_page(
+        'content/blog/majesty/index.md',
+        'template.html',
+        'public/blog/majesty/index.html'
+    )
+    generate_page(
+        'content/contact/index.md',
+        'template.html',
+        'public/contact/index.html'
     )
 
 
