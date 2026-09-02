5885b96217add10409ef130b953f003b9772a3f7
larrymacrich <105505178+larrymacrich@users.noreply.github.com>
2026-09-02 12:29:07 +0200

    finalized full md conversion into html

--- src/blocklevel_markdown.py
+++ src/blocklevel_markdown.py
@@ -1,4 +1,10 @@
 from enum import Enum
+from htmlnode import HTMLNode
+from parentnode import ParentNode
+from leafnode import LeafNode
+from textnode import TextNode, TextType, text_node_to_html_node
+from inline_markdown import text_to_textnodes
+
 class BlockType(Enum):
     PARAGRAPH = "paragraph"
     HEADING = "heading"
@@ -44,3 +50,92 @@ def block_to_block_type(markdown: str) -> BlockType:
     elif valid_ordered_list:
         block_type = BlockType.ORDERED_LIST
     return block_type
+
+def markdown_to_html_node(markdown: str) -> HTMLNode:
+    blocks = markdown_to_blocks(markdown)
+    children = []
+    for block in blocks:
+        block_type = block_to_block_type(block)
+        block_node = block_to_html_node(block_type, block)
+        children.append(block_node)
+    tag = 'div'
+    parent = ParentNode(
+        tag,
+        children
+    )
+    return parent
+
+def block_to_html_node(block_type: BlockType, block: str) -> HTMLNode:
+    match block_type:
+        case BlockType.HEADING:
+            hashes, text = block.split(maxsplit=1)
+            hash_tag = f'h{hashes.count('#')}'
+            children = text_to_children(text)
+            block_node = ParentNode(
+                hash_tag,
+                children,
+            )
+        case BlockType.CODE:
+            text = '\n'.join(block.split('\n')[1:-1])+'\n'
+            child_tag = 'code'
+            child = LeafNode(
+                child_tag,
+                text, 
+            )
+            parent_tag = 'pre'
+            block_node = ParentNode(
+                parent_tag,
+                [child],
+            )
+        case BlockType.QUOTE:
+            quote_tag = 'blockquote'
+            text = ' '.join(
+                line[1:].strip() for line in block.split('\n')
+            )
+            children = text_to_children(text)
+            block_node = ParentNode(
+                quote_tag,
+                children, 
+            )
+        case BlockType.UNORDERED_LIST:
+            child_tag = 'li'
+            children = [
+                ParentNode(
+                    child_tag,
+                    text_to_children(line[1:].strip()),
+                ) for line in block.split('\n')
+            ]
+            parent_tag = 'ul'
+            block_node = ParentNode(
+                parent_tag,
+                children,
+            )
+        case BlockType.ORDERED_LIST:
+            child_tag = 'li'
+            children = [
+                ParentNode(
+                    child_tag,
+                    text_to_children(line.split('.', maxsplit=1)[1].strip()),
+                ) for line in block.split('\n')
+            ]
+            parent_tag = 'ol'
+            block_node = ParentNode(
+                parent_tag,
+                children,
+            )
+        case _: # paragraph
+            tag = 'p'
+            text = ' '.join(block.split('\n'))
+            children = text_to_children(text)
+            block_node = ParentNode(
+                tag,
+                children,
+            )
+    return block_node
+
+def text_to_children(text: str) -> list[HTMLNode]:
+    text_nodes = text_to_textnodes(text)
+    html_nodes = []
+    for text_node in text_nodes:
+        html_nodes.append(text_node_to_html_node(text_node))
+    return html_nodes
--- src/test_blocklevel_markdown.py
+++ src/test_blocklevel_markdown.py
@@ -1,5 +1,13 @@
 import unittest
-from blocklevel_markdown import BlockType, markdown_to_blocks, block_to_block_type
+from parentnode import ParentNode
+from leafnode import LeafNode
+from blocklevel_markdown import (
+    BlockType, 
+    markdown_to_blocks, 
+    block_to_block_type, 
+    text_to_children,
+    markdown_to_html_node,
+)
 
 class TestBlockLevelMarkdownToBlocks(unittest.TestCase):
     """Tests for blocklevel_markdown.markdown_to_blocks()"""
@@ -158,6 +166,100 @@ class TestBlockLevelBlockToBlockTypes(unittest.TestCase):
             block_to_block_type(markdown),
             BlockType.PARAGRAPH
         )
-    
+
+class TestTextToChildren(unittest.TestCase):
+    """Tests for blocklevel_markdown.text_to_children()"""
+    def test_text_to_textnode_all_typed(self):
+        text = (
+            "This is **text** with an _italic_ word and a `code block` " 
+            "and an ![obi wan image](https://i.imgur.com/fJRm4Vk.jpeg) " 
+            "and a [link](https://boot.dev)"
+        )
+        new_nodes = text_to_children(text)
+        self.assertEqual(
+            ''.join(node.to_html() for node in new_nodes),
+            ''.join([
+                LeafNode(None, "This is ").to_html(),
+                LeafNode("b", "text").to_html(),
+                LeafNode(None, " with an ").to_html(),
+                LeafNode("i", "italic").to_html(),
+                LeafNode(None, " word and a ").to_html(),
+                LeafNode("code", "code block").to_html(),
+                LeafNode(None, " and an ").to_html(),
+                LeafNode("img", "", {"src": "https://i.imgur.com/fJRm4Vk.jpeg", "alt": "obi wan image"}).to_html(),
+                LeafNode(None, " and a ").to_html(),
+                LeafNode("a", "link", {"href": "https://boot.dev"}).to_html(),
+            ]),
+        )
+
+class TestBlockMarkdownToHTMLNode(unittest.TestCase):
+    """Tests for blocklevel_markdown.markdown_to_html_node()"""
+    def test_paragraphs(self):
+        md = """
+This is **bolded** paragraph
+text in a p
+tag here
+
+This is another paragraph with _italic_ text and `code` here
+
+"""
+        node = markdown_to_html_node(md)
+        html = node.to_html()
+        self.assertEqual(
+            html,
+            "<div><p>This is <b>bolded</b> paragraph text in a p tag here</p><p>This is another paragraph with <i>italic</i> text and <code>code</code> here</p></div>",
+        )
+
+
+    def test_codeblock(self):
+        md = """
+```
+This is text that _should_ remain
+the **same** even with inline stuff
+```
+"""
+        node = markdown_to_html_node(md)
+        html = node.to_html()
+        self.assertEqual(
+            html,
+            "<div><pre><code>This is text that _should_ remain\nthe **same** even with inline stuff\n</code></pre></div>",
+        )
+
+    def test_heading_level_2(self):
+        md = "## A Heading"
+        node = markdown_to_html_node(md)
+        html = node.to_html()
+        self.assertEqual(html, "<div><h2>A Heading</h2></div>")
+
+    def test_heading_with_inline(self):
+        md = "# A **Bold** Heading"
+        node = markdown_to_html_node(md)
+        html = node.to_html()
+        self.assertEqual(html, "<div><h1>A <b>Bold</b> Heading</h1></div>")
+
+    def test_quote_block(self):
+        md = "> To be or not to be\n> that is the question"
+        node = markdown_to_html_node(md)
+        html = node.to_html()
+        self.assertEqual(html, "<div><blockquote>To be or not to be that is the question</blockquote></div>")
+
+    def test_unordered_list(self):
+        md = "- First\n- Second\n- Third"
+        node = markdown_to_html_node(md)
+        html = node.to_html()
+        self.assertEqual(html, "<div><ul><li>First</li><li>Second</li><li>Third</li></ul></div>")
+
+    def test_ordered_list_double_digits(self):
+        md = "\n".join(f"{i}. Item {i}" for i in range(1, 12))
+        node = markdown_to_html_node(md)
+        html = node.to_html()
+        self.assertEqual(html, "<div><ol><li>Item 1</li><li>Item 2</li><li>Item 3</li><li>Item 4</li><li>Item 5</li><li>Item 6</li><li>Item 7</li><li>Item 8</li><li>Item 9</li><li>Item 10</li><li>Item 11</li></ol></div>")
+
+    def test_mixed_blocks(self):
+        md = "# Title\n\nA paragraph here.\n\n- item one\n- item two"
+        node = markdown_to_html_node(md)
+        html = node.to_html()
+        self.assertEqual(html, "<div><h1>Title</h1><p>A paragraph here.</p><ul><li>item one</li><li>item two</li></ul></div>")
+
 if __name__ == "__main__":
     unittest.main()
